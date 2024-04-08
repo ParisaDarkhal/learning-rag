efe039cc08c1c553b22ac16b16f5b7978a39fd01
ParisaDarkhal <p.darkhal@gmail.com>
2024-04-08 13:21:21 -0400

    agent created

--- backend/index2.js
+++ backend/index2.js
@@ -43,6 +43,52 @@ const response2 = await queryEngine2.query({
 });
 console.log("** " + response2.toString());
 ///////
+// create a function tool
+function sumNumbers({ a, b }) {
+  return a + b;
+}
+const sumJSON = {
+  type: "object",
+  properties: {
+    a: {
+      type: "number",
+      description: "The first number",
+    },
+    b: {
+      type: "number",
+      description: "the second number",
+    },
+  },
+  required: ["a", "b"],
+};
+const sumFunctionTool = new FunctionTool(sumNumbers, {
+  name: "sumNumbers",
+  description: "Use this function to sum two numbers.",
+  parameters: sumJSON,
+});
+////making our previous  query engine into a tool too
+const queryEngineTool = new QueryEngineTool({
+  queryEngine: queryEngine1,
+  queryEngine2,
+  metadata: {
+    name: "neuropharmacology_and_colors_psychology_engine",
+    description:
+      "a tool that can answer questions about neuropharmacology and colors psychology",
+  },
+});
+//make an openAI agent to use these tools
+const agent = new OpenAIAgent({
+  tools: [queryEngineTool, sumFunctionTool],
+  verbose: true,
+});
+//now ask a question
+let response3 = await agent.chat({
+  message: "What is norepinephrine? Use a tool.",
+});
+console.log(response3.toString());
+//now ask llm to do a sum calculation
+let response4 = await agent.chat({ message: "What is 403 + 201?" });
+console.log(response4.toString());
 //////////////
 app.use(express.static("build"));
 app.listen(port, () => {
